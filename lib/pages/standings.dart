5006323064ebe3a938fb95fa1266b2c920726ee3
FriendlyBen <bentleyteh2001@gmail.com>
2023-04-29 01:49:39 +0800

    standing page

--- lib/pages/standings.dart
+++ lib/pages/standings.dart
@@ -3,104 +3,173 @@ import 'package:flutter/material.dart';
 import 'homepage.dart';
 import 'notifications.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStanding(),
-    );
-  }
-}
-
-class MyStanding extends StatefulWidget {
+class MyStanding extends StatelessWidget {
   const MyStanding({Key? key}) : super(key: key);
 
-  @override
-  State<MyStanding> createState() => _MyStandingState();
-}
-
-class _MyStandingState extends State<MyStanding> {
-  int _selectedIndex = 2;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    if (index == 0) {
-      // if the second item is tapped
-      Navigator.pushReplacement(
-        context,
-        PageRouteBuilder(
-            pageBuilder: (context, anim1, anim2) => const MyHomePage(),
-            transitionDuration: Duration.zero),
-      );
-    } else if (index == 1) {
-      // if the second item is tapped
-      Navigator.pushReplacement(
-        context,
-        PageRouteBuilder(
-            pageBuilder: (context, anim1, anim2) => const MyBets(),
-            transitionDuration: Duration.zero),
-        // push the new page onto the stack
-      );
-    } else if (index == 3) {
-      // if the second item is tapped
-      Navigator.pushReplacement(
-        context,
-        PageRouteBuilder(
-            pageBuilder: (context, anim1, anim2) => const MyNotifications(),
-            transitionDuration: Duration.zero),
-      );
-    }
-  }
+  static const int FirstColumn = 8;
+  static const int SecondColumn = 4;
+  static const int ThirdColumn = 2;
+  static const int LastColumn = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-
-        title: const Text('courtside'),
-        automaticallyImplyLeading: false, // set this to false
-      ),
-      body: const Center(
-        child: Text('Bets Page'),
-      ),
-      backgroundColor: Colors.black,
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Colors.black,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Playoff Standings"),
+          centerTitle: true,
         ),
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.paid),
-              label: 'Coin',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.emoji_events),
-              label: 'Trophy',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications),
-              label: 'Notification',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color(0xff43F5BF),
-          unselectedItemColor: Colors.grey,
-          onTap: _onItemTapped,
+        body: ListView.builder(
+          itemCount: 1,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: [
+                    Container(margin: const EdgeInsets.all(10), child: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red)), onPressed: () {}, child: const Text("First 16"), )),
+                    Container(margin: const EdgeInsets.all(10), child: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red)), onPressed: () {}, child: const Text("Quarter Finals"), )),
+                    Container(margin: const EdgeInsets.all(10), child: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red)), onPressed: () {}, child: const Text("Semi Finals"), )),
+                    Container(margin: const EdgeInsets.all(10), child: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red)), onPressed: () {}, child: const Text("Finals"), )),
+
+
+                    // Container(margin: const EdgeInsets.all(10), color: Colors.red, width:30)],
+                  ]
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Column(
+                        children: List.generate(FirstColumn, (index) {
+                          return Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 250,
+                            height: 100,
+                            color: Colors.black,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                      ),
+                      const SizedBox(width: 50 ),
+                      Column(
+                        children: List.generate(SecondColumn, (index) {
+                          return Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 250,
+                            height: 100,
+                            color: Colors.black,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                      ),
+                      const SizedBox(width: 50 ),
+                      Column(
+                        children: List.generate(ThirdColumn, (index) {
+                          return Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 250,
+                            height: 100,
+                            color: Colors.black,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                          );
+
+                        }),
+                      ),
+                      const SizedBox(width: 50 ),
+                      Column(
+                        children: List.generate(LastColumn, (index) {
+                          return Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 250,
+                            height: 100,
+                            color: Colors.black,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                      ),
+                    ],
+                  ),
+                ),
+              BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.paid),
+                    label: 'Coin',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.emoji_events),
+                    label: 'Trophy',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.notifications),
+                    label: 'Notification',
+                  ),
+                ],
+              ),
+            ]
+            );
+          },
+          scrollDirection: Axis.vertical,
         ),
       ),
     );
